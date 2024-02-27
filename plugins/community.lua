3883c947db0d1d04a9d97c6db021dba8bf6ba25a
Flapperoo <eli@garmur.net>
2024-02-27 16:29:44 +0800

    Add initial plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,18 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.media.presence-nvim" },
+  {
+    "andweeb/presence.nvim",
+    opts = {
+      buttons = false,
+    },
+  },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
